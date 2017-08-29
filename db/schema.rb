f0db7ebc2934bd70d10863442f157feec9a2e546
Muhammad Fawwaz <fawyna@gmail.com>
2017-08-29 16:54:35 +0800

    Add images scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829065608) do
+ActiveRecord::Schema.define(version: 20170829084747) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
